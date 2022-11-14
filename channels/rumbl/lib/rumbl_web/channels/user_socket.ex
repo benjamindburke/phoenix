cc2868410ec0adb722491e9a2513fa47e1449b2c
Ben Burke <benjamindburke@gmail.com>
2022-11-14 17:00:12 -0500

    video channel connections

---
+++ channels/rumbl/lib/rumbl_web/channels/user_socket.ex
@@ -0,0 +1,13 @@
+defmodule RumblWeb.UserSocket do
+  use Phoenix.Socket
+
+  ## Channels
+  # channel "room:*", RumblWeb.RoomChannel
+  channel "videos:*", RumblWeb.VideoChannel
+
+  def connect(_params, socket, _connect_info) do
+    {:ok, socket}
+  end
+
+  def id(_socket), do: nil
+end
