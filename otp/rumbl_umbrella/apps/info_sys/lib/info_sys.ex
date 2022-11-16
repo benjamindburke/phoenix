0c5a0d3d57bb9a235f16ffb721ea71e45443155e
Benjamin Burke <benjamindburke@gmail.com>
2022-11-16 02:24:43 -0500

    await mulitple backend tasks

--- otp/rumbl_umbrella/apps/info_sys/lib/info_sys.ex
+++ otp/rumbl_umbrella/apps/info_sys/lib/info_sys.ex
@@ -6,4 +6,28 @@ defmodule InfoSys do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  @backends [InfoSys.Wolfram]
+
+  defmodule Result do
+    defstruct score: 0, text: nil, backend: nil
+  end
+
+  def compute(query, opts \\ []) do
+    opts = Keyword.put_new(opts, :limit, 10)
+    backends = opts[:backends] || @backends
+
+    backends
+    |> Enum.map(&async_query(&1, query, opts))
+  end
+
+  defp async_query(backend, query, opts) do
+    Task.Supervisor.async_nolink(
+      InfoSys.TaskSupervisor,
+      backend,
+      :compute,
+      [query, opts],
+      shutdown: :brutal_kill
+    )
+  end
 end
