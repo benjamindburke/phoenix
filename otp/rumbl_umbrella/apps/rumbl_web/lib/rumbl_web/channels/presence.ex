ceee601157781de45d3117e47562de9838bce0cb
Benjamin Burke <benjamindburke@gmail.com>
2022-11-16 02:05:11 -0500

    mix format

--- otp/rumbl_umbrella/apps/rumbl_web/lib/rumbl_web/channels/presence.ex
+++ otp/rumbl_umbrella/apps/rumbl_web/lib/rumbl_web/channels/presence.ex
@@ -1,7 +1,7 @@
 defmodule RumblWeb.Presence do
   @moduledoc """
   Provides presence tracking to channels and processes.
-
+  
   See the [`Phoenix.Presence`](https://hexdocs.pm/phoenix/Phoenix.Presence.html)
   docs for more details.
   """
